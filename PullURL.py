846424e516a6319c470a248eb6775697be5103bc
Owen Osmera <owenosmera@gmail.com>
2026-03-26 11:51:31 -0600

    Working txt file generation and URL generation

--- PullURL.py
+++ PullURL.py
@@ -5,3 +5,39 @@ PullURL.py
 """
 
 # pip install pytube
+from pytubefix import Playlist
+
+# Defualt paramiters
+DEFAULT_URL = "https://www.youtube.com/playlist?list=PLG49S3nxzAnnOmvg5UGVenB_qQgsh01uC"
+
+
+def gen_link_list(url = DEFAULT_URL):
+    # initialize the playlist object
+    relist = Playlist(url)
+
+    # get all video urls
+    allVids = relist.video_urls
+
+    # create return list
+    result_list = []
+
+    for one in allVids:
+        result_list.append(one)
+
+    return result_list
+        
+
+def main():
+    # hold list
+    hold = []
+
+    # use function with default
+    hold = gen_link_list()
+
+    # print list
+    print(hold)
+
+    
+if __name__ == "__main__":
+    main()
+
--- transcript_1.py
+++ transcript_1.py
@@ -1,21 +1,35 @@
+"""
+transcript_1.py
+3/26/26
+
+"""
+
 from youtube_transcript_api import YouTubeTranscriptApi
 
-# If the video id starts with a hyphen you will have to mask the hyphen
-#Video also needs to have subtitles
+# defualt file name
+DEFUALT = "Example.txt"
+DEFUALT_ID = "111111111"
 
-# youtube_transcript_api "\-abc123"
+# create a usable function
+def gen_readable_file(filename=DEFUALT, id=DEFUALT_ID):
+    # Set object and fetch english captions
+    yta = YouTubeTranscriptApi()
+    fetch = yta.fetch(id, languages=["en"])
 
-# ---Main Code --
+    # create plain text dictionaries
+    tx = fetch.to_raw_data()
 
-outls = []
+    with open(filename, "w", encoding="utf-8") as f:
+        for item in tx:
+            f.write(item["text"] + "\n")
 
-tx = YouTubeTranscriptApi().get_transcript("zODZ0i-Iark", languages=["en"]) 
+def main():
+    # create file with id
+    gen_readable_file("cool.txt", "87t6P5ZHTP0")
 
-for i in tx:
-    outtxt = (i['text'])
-    outls.append(outtxt)
+if __name__ == "__main__":
+    main()
 
-    with open("op.txt", "a") as opf:
-        opf.write(outtxt + "\n")
+
 
 
