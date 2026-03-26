80a322233e904e5db03112e5261996f165401ec7
Owen Osmera <owenosmera@gmail.com>
2026-03-26 11:27:08 -0600

    KeyTest

--- ai_genius.py
+++ ai_genius.py
@@ -1,7 +1,7 @@
 from youtube_transcript_api import YouTubeTranscriptApi
 
 ytt = YouTubeTranscriptApi()
-fetched = ytt.fetch("zODZ0i-Iark", languages=["en"])
+fetched = ytt.fetch("87t6P5ZHTP0", languages=["en"])
 
 # If you want plain list-of-dicts:
 tx = fetched.to_raw_data()
